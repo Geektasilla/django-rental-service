7a2a100fe8ddc22ab9251dc9e8896e7d14dda698
Viktoria <boychenkovika1985@gmail.com>
2026-07-06 20:27:37 +0200

    refactor(users): consolidate package endpoints in models init layout. Closes #9

--- users/models/__init__.py
+++ users/models/__init__.py
@@ -1,3 +1,6 @@
+from .agent import AgentProfile
 from .base_user import User, UserManager
+from .owner import OwnerProfile
+from .tenant import TenantProfile
 
-__all__ = ["User", "UserManager"]
+__all__ = ["User", "UserManager", "OwnerProfile", "AgentProfile", "TenantProfile"]
